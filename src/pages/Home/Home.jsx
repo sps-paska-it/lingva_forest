322344c3d6b2694bba00dcbcf75da8f161ecfa3b
Pavlo <113455386+sps-paska-it@users.noreply.github.com>
2023-09-12 00:26:48 +0200

    update Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import { Container } from 'components/Container/Container';
-import { StyledDiv, StyledH1, TextDiv } from './Home.styled';
+import { StyledDiv, StyledH1, TextDiv, Wrapper } from './Home.styled';
 
 const Home = () => {
   return (
@@ -9,15 +9,24 @@ const Home = () => {
           <StyledH1>
             Школа іноземних мов "Lingva forest" DEUTSCH ENGLISH
           </StyledH1>
-          <TextDiv>
-            <p>
-              Вітаю тебе на своїй міжнародній платформі вивчення німецької для
-              українців. Я - Лілія Хоньків, філологиня і перекладачка, з більш,
-              ніж 10-річним досвідом роботи у сфері онлайн-освіти. Переходь на
-              мої соцмережі, де щодня наше живе спілкування. Чекаю тебе на своїх
-              курсах - тут твоє щоденне зростання.
-            </p>
-          </TextDiv>
+          <Wrapper>
+            <TextDiv>
+              <p>
+                Вітаю тебе на своїй міжнародній платформі вивчення німецької для
+                українців. Я - Лілія Хоньків, філологиня і перекладачка, з
+                більш, ніж 10-річним досвідом роботи у сфері онлайн-освіти.
+                Переходь на мої соцмережі, де щодня наше живе спілкування. Чекаю
+                тебе на своїх курсах - тут твоє щоденне зростання.
+              </p>
+            </TextDiv>
+            <img
+              width="500px"
+              class="image-boxes-img img-responsive "
+              src="https://wow-deutsch.com/wp-content/uploads/2023/04/sajt-1.png"
+              alt="phot"
+              title="photo"
+            ></img>
+          </Wrapper>
         </Container>
       </StyledDiv>
     </>
--- src/pages/Home/Home.styled.js
+++ src/pages/Home/Home.styled.js
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 export const StyledDiv = styled.div`
   margin-top: 85px;
   /* border-top: 2px solid rgba(34, 139, 34); */
-  height: 500px;
+  height: 800px;
   background-color: rgb(180, 255, 160);
 `;
 
@@ -14,9 +14,15 @@ export const StyledH1 = styled.h1`
   width: 380px;
 `;
 
+export const Wrapper = styled.div`
+  display: flex;
+  column-gap: 50px;
+`;
+
 export const TextDiv = styled.div`
-  margin-top: 75px;
-  width: 500px;
+  margin-top: 150px;
+  width: 700px;
+  height: 100px;
   padding: 15px;
   border-radius: 15px;
   background-color: #fff;
